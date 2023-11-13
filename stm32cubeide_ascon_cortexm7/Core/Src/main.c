42d433171ffa87821ef25604e7f2aa42bab3f658
mrabie19 <m.rabie-19@lboro.ac.uk>
2023-11-13 16:11:18 +0000

    Added TinyJAMBU Xoodyak and timer interrupts to measure performance

--- stm32cubeide_ascon_cortexm7/Core/Src/main.c
+++ stm32cubeide_ascon_cortexm7/Core/Src/main.c
@@ -17,30 +17,38 @@
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
-
-//#define VERSION 128
-
-#ifdef VERSION == version128
-#include <ASCON-128/aead.h>
-#define ENCRYPT(a,b,c,d,e,f,g,h,i) crypto_aead_encrypt(a,b,c,d,e,f,g,h,i)
-#define DECRYPT(a,b,c,d,e,f,g,h,i) crypto_aead_decrypt(a,b,c,d,e,f,g,h,i)
-#else
-#include <ASCON-128a/aead.h>
-#define ENCRYPT(a,b,c,d,e,f,g,h,i) crypto_aead_encrypta(a,b,c,d,e,f,g,h,i)
-#define DECRYPT(a,b,c,d,e,f,g,h,i) crypto_aead_decrypta(a,b,c,d,e,f,g,h,i)
-#endif
-
 #include "main.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-//#include "stdio.h"
-#include "string.h"
+
+#define ASCON128 0
+#define ASCON128A 1
+#define TINYJAMBU 2
+#define XOODYAK 3
+#define MSG_SIZE 16
+
+#define ENCRYPT(a,b,c,d,e,f,g,h,i) crypto_aead_encrypt(a,b,c,d,e,f,g,h,i)
+#define DECRYPT(a,b,c,d,e,f,g,h,i) crypto_aead_decrypt(a,b,c,d,e,f,g,h,i)
+
+#define VERSION XOODYAK
+
+#if VERSION == ASCON128
+	#include <ASCON-128/aead.h>
+#elif VERSION == ASCON128A
+	#include <ASCON-128a/aead.h>
+#elif VERSION == TINYJAMBU
+	#include <lwc_crypto_aead.h>
+	#include <TinyJAMBU/api.h>
+#elif VERSION == XOODYAK
+	#include <Xoodyak/Xoodyak.h>
+#endif
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-typedef unsigned long long bit64;
+//typedef unsigned long long bit64;
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
@@ -55,112 +63,25 @@ typedef unsigned long long bit64;
 
 /* Private variables ---------------------------------------------------------*/
 
-TIM_HandleTypeDef htim14;
+TIM_HandleTypeDef htim6;
+TIM_HandleTypeDef htim7;
 
 /* USER CODE BEGIN PV */
-// 320-bit state size
-//bit64 state[5] = {0}, t[5] = {0};
-//bit64 constants[12] = {
-//		0xf0, 0xe1, 0xd2, 0xc3, 0xb4, 0xa5,
-//		0x96, 0x87, 0x78, 0x69, 0x5a, 0x4b
-//};
+int etimer_overflow = -1;
+int dtimer_overflow = -1;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_TIM14_Init(void);
+static void MX_TIM6_Init(void);
+static void MX_TIM7_Init(void);
 /* USER CODE BEGIN PFP */
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-// Required for printf
-//int _write(int file, char *ptr, int len){
-//	int i = 0;
-//	for (i=0;i<len;i++){
-//		ITM_SendChar((*ptr++));
-//	}
-//	return len;
-//}
-//
-//bit64 rotate(bit64 x, int l){
-//	bit64 temp;
-//	temp = (x>>l) ^ (x<<(64-l));
-//	return temp;
-//}
-//
-//void linear(bit64 state[5]){
-//	bit64 temp0, temp1;
-//
-//	temp0 = rotate(state[0], 19);
-//	temp1 = rotate(state[0], 28);
-//	state[0] ^= temp0 ^ temp1;
-//
-//	temp0 = rotate(state[0], 61);
-//	temp1 = rotate(state[0], 39);
-//	state[1] ^= temp0 ^ temp1;
-//
-//	temp0 = rotate(state[0], 1);
-//	temp1 = rotate(state[0], 6);
-//	state[2] ^= temp0 ^ temp1;
-//
-//	temp0 = rotate(state[0], 10);
-//	temp1 = rotate(state[0], 17);
-//	state[3] ^= temp0 ^ temp1;
-//
-//	temp0 = rotate(state[0], 7);
-//	temp1 = rotate(state[0], 41);
-//	state[4] ^= temp0 ^ temp1;
-//}
-//
-//void sbox(bit64 x[5]){
-//	// bit slice implementation
-//	x[0] ^= x[4]; x[4] ^= x[3]; x[2] ^= x[1];
-//	t[0] = x[0]; t[1] = x[1]; t[2] = x[2]; t[3] = x[3]; t[4] = x[4];
-//	t[0] =~ t[0]; t[1] =~ t[1]; t[2] =~ t[2]; t[3] =~ t[3]; t[4] =~ t[4];
-//	t[0] &= x[1]; t[1] &= x[2]; t[2] &= x[3]; t[3] &= x[4]; t[4] &= x[0];
-//	x[0] ^= t[1]; x[1] ^= t[2]; x[2] ^= t[3]; x[3] ^= t[4]; x[4] ^= t[0];
-//	x[1] ^= x[0]; x[0] ^= x[4]; x[3] ^= x[2]; x[2] =~ x[2];
-//}
-//
-//void add_constant(bit64 state[5], int i, int a){
-//	state[2] ^= constants[12 - a - i];
-//}
-//
-//void p(bit64 state[5], int a){
-//	for(int i=0; i<a; i++){
-//		add_constant(state, i, a);
-//		sbox(state);
-//		linear(state);
-//	}
-//}
-//
-//void initialisation(bit64 state[5], bit64 key[2]){
-//	p(state, 12);
-//	state[3] ^= key[0];
-//	state[4] ^= key[1];
-//}
-//
-//void print_states(bit64 state[5]){
-//	for(int i = 0; i<5; i++) printf("0x%llx\n", state[i]);
-//}
-//
-//void encrypt(bit64 state[5], int length, bit64 plaintext[], bit64 ciphertext[]){
-//	for(int i =0; i<length; i++){
-//		p(state, 6);
-//		ciphertext[i] = plaintext[i] ^ state[0];
-//		state[0] = ciphertext[i];
-//	}
-//}
-//
-//void finalisation(bit64 state[5], bit64 key[2], bit64 tag[2]){
-//	state[0] ^= key[0];
-//	state[1] ^= key[1];
-//	p(state, 12);
-//	tag[0] = state[3] ^ key[0];
-//	tag[1] = state[4] ^ key[1];
-//}
+
 
 /* USER CODE END 0 */
 
@@ -171,7 +92,8 @@ static void MX_TIM14_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-	uint16_t timer_val, timer_val_d;
+	uint32_t timer6_initial = 0, timer6_rem = 0, timer_enc = 0, timer_dec = 0;
+	uint32_t timer7_initial = 0, timer7_rem = 0;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -192,46 +114,26 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_TIM14_Init();
+  MX_TIM6_Init();
+  MX_TIM7_Init();
   /* USER CODE BEGIN 2 */
 
-  /*
-  bit64 nonce[2] = {0};
-  bit64 key[2] = {0};
-  bit64 IV = 0x80400c0600000000;
-  bit64 plaintext[] = {0x123456789abcdef, 0x34543523213}, ciphertext[10] = {0};
-  bit64 tag[2] = {0};
-  state[0] = IV;
-  state[1] = key[0];
-  state[2] = key[1];
-  state[3] = nonce[0];
-  state[4] = nonce[1];
-  initialisation(state, key);
-  print_states(state);
-  encrypt(state, sizeof(plaintext)/sizeof(bit64), plaintext, ciphertext);
-  finalisation(state, key, tag);
-  */
-
-//  volatile unsigned char *stack;
-//  stack = 0x20000000;
-//  volatile unsigned char mystack[500000] = {0xbe};
-//  for (int i = 0; i<500000; i++)
-//	  mystack[i]= 0xbe;
-
-  volatile unsigned char nonce[16] = {0xf0, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
-  volatile unsigned char key[16] =  {0xf0, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
-#ifdef VERSION == 128
-  volatile unsigned char msg[8] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef};
-#else
-  volatile unsigned char msg[16] = {
-		  0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
-		  0x1, 0x3, 0x5, 0x7, 0x9, 0xb, 0xd, 0xf
-  };
-#endif
-  volatile unsigned char dt[8] = {0};
+  volatile unsigned char nonce[CRYPTO_NPUBBYTES] = {0};
+  volatile unsigned char key[CRYPTO_KEYBYTES] =  {0};
+//  volatile unsigned char nonce[16] = {0}; volatile unsigned char key[16] =  {0};
+//#if VERSION == 128
+  volatile unsigned char msg[MSG_SIZE] = {0x0};
+//#else
+//  volatile unsigned char msg[MSG_SIZE] = {
+//		  0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
+//		  0x1, 0x3, 0x5, 0x7, 0x9, 0xb, 0xd, 0xf
+//  };
+//#endif
+  volatile unsigned char dt[MSG_SIZE] = {0};
   volatile uint64_t msglen = sizeof(msg)/sizeof(unsigned char);
-  volatile unsigned long long ctlen;
-  volatile unsigned char ct[50] = {0};
+  volatile unsigned long long ctlen = 0;
+  volatile unsigned char ct[MSG_SIZE + CRYPTO_ABYTES] = {0};
+  volatile unsigned long long adlen = 0;
 
   // Declare pointers
   volatile unsigned char *c;
@@ -249,13 +151,35 @@ int main(void)
   m = msg;
   dm = dt;
   c = ct;
-  HAL_TIM_Base_Start(&htim14);
-  // ASCON functions
-  timer_val = __HAL_TIM_GET_COUNTER(&htim14);
-  ENCRYPT(c, clen, m, msglen, NULL, 0, NULL, npub, k);
-  timer_val = __HAL_TIM_GET_COUNTER(&htim14) - timer_val;
-  DECRYPT(dm, mlen, NULL, c, ctlen, NULL, 0, npub, k);
-  timer_val_d = __HAL_TIM_GET_COUNTER(&htim14) - timer_val;
+
+  // Encrypt
+  HAL_TIM_Base_Start_IT(&htim6);
+  timer6_initial = __HAL_TIM_GET_COUNTER(&htim6);
+  ENCRYPT(c, clen, m, msglen, NULL, adlen, NULL, npub, k);
+  timer6_rem = __HAL_TIM_GET_COUNTER(&htim6);
+  if(etimer_overflow){
+	  int temp;
+	  temp = 65535 - timer6_initial;
+	  etimer_overflow--;
+	  timer_enc = temp + timer6_rem + etimer_overflow*65535;
+  } else {
+	  timer_enc = timer6_rem;
+  }
+
+  // Decrypt
+  HAL_TIM_Base_Start_IT(&htim7);
+  timer7_initial = __HAL_TIM_GET_COUNTER(&htim7);
+  DECRYPT(dm, mlen, NULL, c, ctlen, NULL, adlen, npub, k);
+  timer7_rem = __HAL_TIM_GET_COUNTER(&htim7);
+  if(dtimer_overflow){
+	  int temp;
+	  temp = 65535 - timer7_initial;
+	  dtimer_overflow--;
+	  timer_dec = temp + timer7_rem + dtimer_overflow*65535;
+  } else {
+	  timer_dec = timer7_rem;
+  }
+
 
   /* USER CODE END 2 */
 
@@ -279,10 +203,14 @@ void SystemClock_Config(void)
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
+  /** Configure LSE Drive Capability
+  */
+  HAL_PWR_EnableBkUpAccess();
+
   /** Configure the main internal regulator output voltage
   */
   __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -290,21 +218,8 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 16;
-  RCC_OscInitStruct.PLL.PLLN = 432;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 2;
-  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Activate the Over-Drive mode
-  */
-  if (HAL_PWREx_EnableOverDrive() != HAL_OK)
   {
     Error_Handler();
   }
@@ -313,45 +228,90 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV8;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
   {
     Error_Handler();
   }
 }
 
 /**
-  * @brief TIM14 Initialization Function
+  * @brief TIM6 Initialization Function
   * @param None
   * @retval None
   */
-static void MX_TIM14_Init(void)
+static void MX_TIM6_Init(void)
 {
 
-  /* USER CODE BEGIN TIM14_Init 0 */
+  /* USER CODE BEGIN TIM6_Init 0 */
 
-  /* USER CODE END TIM14_Init 0 */
+  /* USER CODE END TIM6_Init 0 */
 
-  /* USER CODE BEGIN TIM14_Init 1 */
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
 
-  /* USER CODE END TIM14_Init 1 */
-  htim14.Instance = TIM14;
-  htim14.Init.Prescaler = 0;
-  htim14.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim14.Init.Period = 65535;
-  htim14.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim14.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim14) != HAL_OK)
+  /* USER CODE BEGIN TIM6_Init 1 */
+
+  /* USER CODE END TIM6_Init 1 */
+  htim6.Instance = TIM6;
+  htim6.Init.Prescaler = 319;
+  htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim6.Init.Period = 65535;
+  htim6.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim6) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN TIM14_Init 2 */
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim6, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM6_Init 2 */
 
-  /* USER CODE END TIM14_Init 2 */
+  /* USER CODE END TIM6_Init 2 */
+
+}
+
+/**
+  * @brief TIM7 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM7_Init(void)
+{
+
+  /* USER CODE BEGIN TIM7_Init 0 */
+
+  /* USER CODE END TIM7_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM7_Init 1 */
+
+  /* USER CODE END TIM7_Init 1 */
+  htim7.Instance = TIM7;
+  htim7.Init.Prescaler = 319;
+  htim7.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim7.Init.Period = 65535;
+  htim7.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim7) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim7, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM7_Init 2 */
+
+  /* USER CODE END TIM7_Init 2 */
 
 }
 
@@ -362,19 +322,44 @@ static void MX_TIM14_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 /* USER CODE BEGIN MX_GPIO_Init_1 */
 /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PB3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  // Check which version of the timer triggered this callback and toggle LED
+  if (htim == &htim6 )
+  {
+	  etimer_overflow++;
+  }
+
+  // Check which version of the timer triggered this callback and toggle LED
+  else if (htim == &htim7 )
+  {
+	  dtimer_overflow++;
+  }
+}
 
 /* USER CODE END 4 */
 
